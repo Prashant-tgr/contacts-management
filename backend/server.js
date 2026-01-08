0dfdb0d94d3a050f4283e48889fc7a34c5207bb4
Prashant-tgr <prashant.14219011622@ipu.ac.in>
2026-01-08 23:01:48 +0530

    fix: frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const contactRoutes = require('./routes/contacts');
 app.use('/api/contacts', contactRoutes);
-
+/* *
 // Serve built frontend
 const distPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(distPath));
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
-
+*/
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/contacts_db';
 
